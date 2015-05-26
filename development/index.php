6f1aa2c82811c2f27a89a41847c2f4a76bbc3082
Rosemary <perkinsrosemaryl@gmail.com>
2015-05-25 17:18:39 -0700

    Add placeholder content to index.php

--- development/index.php
+++ development/index.php
@@ -7,6 +7,38 @@
 	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
-	
+	<header>
+	<!-- Position logo absolute so it can hang over the nav -->
+	<img src="http://lorempixel.com/160/220/" alt="Placeholder logo">
+		<h1>League of United Lating American Citizens</h1>
+		<h2>Southwest Washington Council #47013</h2>
+	<nav>
+		<ul>
+			<li><a href="national.php">National</a></li>
+			<li><a href="officers.php">Officers</a></li>
+			<li><a href="youth.php">Youth</a></li>
+			<li><a href="community.php">Community</a></li>
+			<li><a href="events.php">Events</a></li>
+		</ul>
+	</nav>
+	</header>
+
+	<!-- Three columns -->
+	<div class="three-col-container">
+		<section class="three-col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque reprehenderit nulla quia numquam explicabo natus illum eveniet, cumque nostrum iure quibusdam iusto placeat esse libero, consectetur eius consequuntur deserunt.</section>
+		<section class="three-col">
+			<img src="http://lorempixel.com/220/160/" alt="Placeholder logo">
+		</section>
+		<section class="three-col">
+			<img src="http://lorempixel.com/160/220/" alt="Placeholder logo">
+		</section>
+	</div>
+
+	<div class="two-col-container">
+		<section class="thirty-sixty">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quo magni doloremque libero et quisquam, aliquam, sapiente sint modi soluta expedita placeat fugiat minus repellat architecto iure atque eius voluptatum.
+		</section>
+		<section class="thirty-sixty"></section>
+	</div>
 </body>
 </html>
