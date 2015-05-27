60de5e13b81ca4b6a89d75981f59cdf3007ebc45
Rosemary <perkinsrosemaryl@gmail.com>
2015-05-26 22:40:31 -0700

    Finish layout and placeholder content for index.php
    Remove community.php, replace with partners.php

--- development/index.php
+++ development/index.php
@@ -9,36 +9,60 @@
 <body>
 	<header>
 	<!-- Position logo absolute so it can hang over the nav -->
-	<img src="http://lorempixel.com/160/220/" alt="Placeholder logo">
-		<h1>League of United Lating American Citizens</h1>
-		<h2>Southwest Washington Council #47013</h2>
+	<img src="images/logo.png" alt="LULAC Logo">
+		<div class="heading-wrapper">
+			<h1>League of United Latin American Citizens</h1>
+			<h2>Southwest Washington Council #47013</h2>
+		</div>
 	<nav>
+		<a href="#" class="menuIcon">Menu</a>
 		<ul>
 			<li><a href="national.php">National</a></li>
 			<li><a href="officers.php">Officers</a></li>
 			<li><a href="youth.php">Youth</a></li>
-			<li><a href="community.php">Community</a></li>
+			<li><a href="partners.php">Partners</a></li>
 			<li><a href="events.php">Events</a></li>
 		</ul>
 	</nav>
 	</header>
 
+<div class="content">
 	<!-- Three columns -->
 	<div class="three-col-container">
-		<section class="three-col">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque reprehenderit nulla quia numquam explicabo natus illum eveniet, cumque nostrum iure quibusdam iusto placeat esse libero, consectetur eius consequuntur deserunt.</section>
 		<section class="three-col">
-			<img src="http://lorempixel.com/220/160/" alt="Placeholder logo">
+			<h2>Mission</h2>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque reprehenderit nulla quia numquam explicabo natus illum eveniet, cumque nostrum iure quibusdam iusto placeat esse libero, consectetur eius consequuntur deserunt.
 		</section>
 		<section class="three-col">
-			<img src="http://lorempixel.com/160/220/" alt="Placeholder logo">
+			<h2>Community Involvement</h2>
+			<img src="http://lorempixel.com/220/160/" alt="Placeholder logo" class="full-img">
+		</section>
+		<section class="three-col">
+			<h2>Partners</h2>
+			<img src="http://lorempixel.com/160/220/" alt="Placeholder logo" class="full-img">
 		</section>
 	</div>
 
-	<div class="two-col-container">
-		<section class="thirty-sixty">
+	<div class="thirty-sixty-container">
+		<section class="thirty">
+			<h2>History</h2>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quo magni doloremque libero et quisquam, aliquam, sapiente sint modi soluta expedita placeat fugiat minus repellat architecto iure atque eius voluptatum.
 		</section>
-		<section class="thirty-sixty"></section>
+		<section class="sixty">...&gt;</section>
 	</div>
+</div>
+
+<footer>
+	<nav>
+		<ul>
+			<li><a href="national.php">National</a></li>
+			<li><a href="officers.php">Officers</a></li>
+			<li><a href="youth.php">Youth</a></li>
+			<li><a href="partners.php">Partners</a></li>
+			<li><a href="events.php">Events</a></li>
+		</ul>
+	</nav>
+</footer>
+
 </body>
 </html>
