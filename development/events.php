4ebb3c21c1d0f849233de5911e5467a92280d6fb
Rosemary <perkinsrosemaryl@gmail.com>
2015-05-28 22:46:11 -0700

    Finished events.php

--- development/events.php
+++ development/events.php
@@ -3,5 +3,13 @@ $page_title = "Events";
 $page_heading = "Events";
 require_once "includes/header.inc.php";
  ?>
-	
+<section class="events">
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi fuga recusandae tenetur vel, doloremque debitis aperiam odit voluptatum  dicta saepe quod voluptates!
+	</p>
+
+	<iframe src="https://www.google.com/calendar/embed?src=s1bd17rencvlpumdfdchqat75c%40group.calendar.google.com&ctz=America/Los_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+
+</section>
+
  <?php require_once "includes/footer.inc.php"; ?>
