1a7b2a4d88deb073db89530ead1b386bac2ce8f2
Rosemary <perkinsrosemaryl@gmail.com>
2015-05-29 18:21:41 -0700

    Finish youth.php desktop layout

--- development/youth.php
+++ development/youth.php
@@ -3,5 +3,35 @@ $page_title = "Youth";
 $page_heading = "Youth";
 require_once "includes/header.inc.php";
  ?>
-	
+	<section class="youth">
+		<h3>
+			Heading 3
+		</h3>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus sunt, debitis sit facere recusandae ratione, harum amet autem aliquid quisquam neque, aspernatur laudantium minima dolore, cumque fugit incidunt alias? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta alias, eaque labore consectetur fugit odit, sint, rem amet et sapiente eos omnis quasi nemo tempore hic fugiat. Possimus voluptatem, assumenda!
+		</p>
+
+		<div class="thirty-sixty-container">
+			<img src="http://lorempixel.com/220/220/" alt="Placeholder image" class="full-img">
+			<img src="http://lorempixel.com/460/230/" alt="Placeholder image" class="full-img">
+
+		</div>
+	</section>
+	<div class="clearFix"></div>
+
+	<section class="youth">
+		<h3>
+			Heading 3
+		</h3>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae culpa expedita iusto vitae nobis deleniti eum, impedit natus asperiores, a, commodi vero enim assumenda velit eos corporis debitis doloremque veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam quaerat eaque unde vitae, quos provident obcaecati iure, eius voluptates? Dolorem, porro? Dolore quas magnam sint distinctio culpa, reiciendis doloremque ad.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto odit tempora corporis modi tempore unde, cum commodi, qui sunt harum. Odit voluptatem repellendus nulla voluptatibus asperiores corporis labore, sint autem. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis quisquam cumque repellat eum voluptates dolore, totam deserunt quaerat tenetur harum possimus nemo nobis sunt molestias, pariatur sint distinctio a consequatur.
+		</p>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium placeat corrupti esse deserunt fugiat. Culpa optio magni in temporibus fugit esse, perferendis ipsam, ab sit ducimus, dolor atque minus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. In alias, quam facere. Sit maiores, quibusdam tenetur aliquam, veritatis officia, voluptatum quia veniam, doloremque sunt consequuntur mollitia dolor laborum culpa. Accusantium.
+		</p>
+	</section>
  <?php require_once "includes/footer.inc.php"; ?>
