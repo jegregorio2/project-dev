a0198c260f1c84bae5a865ebc85901ef6b8b147e
Rosemary <perkinsrosemaryl@gmail.com>
2015-05-29 18:41:03 -0700

    Finished national.php structure

--- development/national.php
+++ development/national.php
@@ -3,5 +3,39 @@ $page_title = "National";
 $page_heading = "National";
 require_once "includes/header.inc.php";
  ?>
-	
+	<div class="fifty-fifty-container">
+		<section>
+			<h3>
+				Mission
+			</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates quidem aspernatur laborum officiis, distinctio at fugit corrupti quas nam. Voluptas sint nisi totam tempora eligendi voluptatibus praesentium unde tenetur assumenda.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta, molestias culpa eveniet temporibus voluptatum quod non ipsa nulla dolorem cum, id, dignissimos aliquam nostrum est repellendus fugiat consequatur. 
+			</p>
+		</section>
+
+		<img src="http://lorempixel.com/260/180/" alt="Placeholder image" class="full-img">
+	</div>
+
+	<div class="fifty-fifty-container">
+		<section>
+			<h3>
+				History
+			</h3>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates quidem aspernatur laborum officiis, distinctio at fugit corrupti quas nam. Voluptas sint nisi totam tempora eligendi voluptatibus praesentium unde tenetur assumenda.
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente totam temporibus, iste molestiae. Corporis placeat officiis tempore beatae nisi quia nemo voluptatibus, dolore quaerat officia soluta ducimus minus. Eaque, doloremque!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque quas excepturi ipsam aliquam ad, repellat accusamus provident consequuntur dolor labore ipsum accusantium. Perspiciatis adipisci sint repellendus! Autem eum nesciunt iste!
+			</p>
+		</section>
+		<img src="http://lorempixel.com/260/180/" alt="Placeholder image" class="full-img">
+		<img src="http://lorempixel.com/260/180/" alt="Placeholder image" class="full-img">
+	</div>
  <?php require_once "includes/footer.inc.php"; ?>
