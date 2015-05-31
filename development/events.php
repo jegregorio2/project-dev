fb9bb185304bb9adf57252b6d3f314728f95b2b7
Rosemary <perkinsrosemaryl@gmail.com>
2015-05-30 22:40:19 -0700

    Add upcoming event to events.php

--- development/events.php
+++ development/events.php
@@ -8,6 +8,15 @@ require_once "includes/header.inc.php";
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi fuga recusandae tenetur vel, doloremque debitis aperiam odit voluptatum  dicta saepe quod voluptates!
 	</p>
 
+	<section>
+		<h3>
+			Upcoming Event Title
+		</h3>
+		<p>
+			<strong>April 13-15 2016</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non assumenda asperiores odio facilis sint sequi obcaecati impedit, voluptatum iste aspernatur blanditiis minima similique fugiat debitis ea beatae modi reprehenderit deserunt!
+		</p>
+	</section>
+
 	<iframe src="https://www.google.com/calendar/embed?src=s1bd17rencvlpumdfdchqat75c%40group.calendar.google.com&ctz=America/Los_Angeles" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
 </section>
